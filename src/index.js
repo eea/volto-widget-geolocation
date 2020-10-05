7294b6bb86e3ea1e6c7fa2f3b700f14033d23ae6
nileshgulia1 <nileshgulia@gmail.com>
2020-10-05 23:42:57 +0530

    add base GeolocationWidget

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import { GeolocationWidget } from './components';
+
 const applyConfig = (config) => {
+  config.widgets.type = {
+    ...config.widgets.type,
+    boolean: GeolocationWidget,
+  };
   return config;
 };
 
