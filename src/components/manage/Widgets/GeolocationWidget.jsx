0c76b0352468c58435ce8641f1a0d054fc5db41c
nileshgulia1 <nileshgulia@gmail.com>
2020-10-09 22:07:02 +0530

    remove unused stuffs

--- src/components/manage/Widgets/GeolocationWidget.jsx
+++ src/components/manage/Widgets/GeolocationWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
 import { defineMessages, injectIntl } from 'react-intl';
