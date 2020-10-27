96ddaeba2cc791fd5e233abbfa3b5e99a6316843
nileshgulia1 <nileshgulia@gmail.com>
2020-10-28 01:25:45 +0530

    refactor urlGetter

--- src/components/manage/Widgets/util.js
+++ src/components/manage/Widgets/util.js
@@ -6,58 +6,32 @@ export function getCountryCode(countryName = '') {
 
 export function makeSearchUrl(data, text, password, countryCode) {
   const { featureClass, continents = '' } = data;
+  const filterFeature = {
+    country: 'A',
+    stream: 'H',
+    parks: 'L',
+    city: 'P',
+    road: 'R',
+    spot: 'S',
+    mountain: 'T',
+    undersea: 'U',
+    forest: 'V',
+  };
   if (featureClass) {
-    switch (true) {
-      case featureClass.startsWith('country'):
+    let url = Object.keys(filterFeature).map((item) => {
+      if (featureClass.startsWith(item)) {
         return `https://secure.geonames.org/searchJSON?q=${text}&country=${
           countryCode || ''
-        }&featureClass=A&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('stream'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=H&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('parks'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=L&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('city'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=P&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('road'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=R&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('spot'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=S&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('mountain'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=T&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('undersea'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=U&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      case featureClass.startsWith('forest'):
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
-        }&featureClass=V&continentCode=${continents}&maxRows=10&username=${password}`;
-
-      default:
-        return `https://secure.geonames.org/searchJSON?q=${text}&country=${
-          countryCode || ''
+        }&featureClass=${
+          filterFeature[item]
         }&continentCode=${continents}&maxRows=10&username=${password}`;
-    }
+      }
+    });
+    if (url.every((item) => item === undefined))
+      return `https://secure.geonames.org/searchJSON?q=${text}&country=${
+        countryCode || ''
+      }&continentCode=${continents}&maxRows=10&username=${password}`;
+    return url.find((item) => item !== undefined);
   }
   return `https://secure.geonames.org/searchJSON?q=${text}&country=${
     countryCode || ''
