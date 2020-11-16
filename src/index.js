1b1f9adc705bbec8160fad140ec3bc2fe08f61de
Nilesh <nileshgulia@gmail.com>
2020-11-17 01:11:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const applyConfig = (config) => {
   config.settings.allowed_cors_destinations = [
     ...(config.settings.allowed_cors_destinations || []),
     'secure.geonames.org',
-    'alin.dev2aws.eea.europa.eu',
   ];
 
   config.addonReducers = {
