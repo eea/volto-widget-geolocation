c38d92dbd4fbac9b0c846641124dfe59449e7d70
EEA Jenkins <2368628+eea-jenkins@users.noreply.github.com>
2024-04-18 16:59:50 +0300

    feat: Volto 17 support - refs #264527

--- src/components/manage/Widgets/SearchGeoName.jsx
+++ src/components/manage/Widgets/SearchGeoName.jsx
@@ -11,7 +11,7 @@ import worldSVG from '@plone/volto/icons/world.svg';
 import clearSVG from '@plone/volto/icons/clear.svg';
 import checkSVG from '@plone/volto/icons/check.svg';
 
-export default (props) => {
+const SearchGeoName = (props) => {
   const { id, data, block, setPopup, onChange, onChangeSchema } = props;
   const [resultsValue, setResultsValue] = React.useState(data.geolocation);
   const InlineFormSchema = schema(props);
@@ -119,3 +119,5 @@ export default (props) => {
     />
   );
 };
+
+export default SearchGeoName;
