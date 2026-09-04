7b69b219d04dd01ee5ab2d9e243baa016e485694
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2026-09-04 12:01:27 +0300

    fix: satisfy Betterleaks scan

--- packages/volto-widget-geolocation/src/components/manage/Widgets/SearchWidget.jsx
+++ packages/volto-widget-geolocation/src/components/manage/Widgets/SearchWidget.jsx
@@ -39,8 +39,7 @@ const SearchWidget = (props) => {
   const { countries } = data;
   const [text, setText] = useState('');
   const dispatch = useDispatch();
-  const password = useSelector(
-    //betterleaks:allow
+  const password = useSelector( // betterleaks:allow -- Redux state reference
     (state) => state.geolocation?.api?.geonames.password,
   );
 
